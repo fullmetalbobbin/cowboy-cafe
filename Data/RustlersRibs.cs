92c431976ebf7c10eae6d654ef908b2f1285bb7b
Amanda Dreesen <amandadreesen@ALaptopHasNoName.local>
2020-01-29 19:39:26 -0600

    added rustlersribs and tested

---
+++ Data/RustlersRibs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CowboyCafe.Data
+{
+    public class RustlersRibs
+    {
+        /// <summary>
+        /// The price of the RustlersRibs
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 7.50;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the RustlersRibs
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 894;
+            }
+        }
+
+        /// <summary>
+        /// Special instructions for the preparation of the RustlersRibs
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                return instructions;
+            }
+        }
+
+    }
+}
