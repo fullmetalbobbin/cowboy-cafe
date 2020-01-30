60889697c94bc0853296d78d8ad71eb3e1eeeb75
Amanda Dreesen <amandadreesen@ALaptopHasNoName.local>
2020-01-29 18:52:59 -0600

    added angry chicken class and tested

---
+++ Data/AngryChicken.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CowboyCafe.Data
+{
+    public class AngryChicken
+    {
+
+        private bool bread = true;
+        /// <summary>
+        /// If the AngryChicken sammich has bread
+        /// </summary>
+        public bool Bread
+        {
+            get { return bread; }
+            set { bread = value; }
+        }
+
+        private bool pickle = true;
+        /// <summary>
+        /// If the AngryChicken sammich has pickle
+        /// </summary>
+        public bool Pickle
+        {
+            get { return pickle; }
+            set { pickle = value; }
+        }
+
+        /// <summary>
+        /// The price of the AngryChicken sammich
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 5.99;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the AngryChicken sammich
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 190;
+            }
+        }
+
+        /// <summary>
+        /// Special instructions for the preparation of the AngryChicken sammich
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!bread) instructions.Add("hold bread");
+                if (!pickle) instructions.Add("hold pickle");
+
+                return instructions;
+            }
+        }
+
+
+
+     }
+    
+}
