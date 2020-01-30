b706c364b1797f83e44abaa37b96de9cba916f25
Amanda Dreesen <amandadreesen@ALaptopHasNoName.local>
2020-01-29 19:12:14 -0600

    added trailburger class and tested

---
+++ Data/Trailburger.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CowboyCafe.Data
+{
+    public class TrailBurger
+    {
+
+        private bool bun = true;
+        /// <summary>
+        /// If the TrailBurger has bun
+        /// </summary>
+        public bool Bun
+        {
+            get { return bun; }
+            set { bun = value; }
+        }
+
+        private bool ketchup = true;
+        /// <summary>
+        /// If the TrailBurger has ketchup
+        /// </summary>
+        public bool Ketchup
+        {
+            get { return ketchup; }
+            set { ketchup = value; }
+        }
+
+        private bool mustard = true;
+        /// <summary>
+        /// If the TrailBurger has mustard
+        /// </summary>
+        public bool Mustard
+        {
+            get { return mustard; }
+            set { mustard = value; }
+        }
+
+        private bool pickle = true;
+        /// <summary>
+        /// If the TrailBurger has pickle
+        /// </summary>
+        public bool Pickle
+        {
+            get { return pickle; }
+            set { pickle = value; }
+        }
+
+        private bool cheese = true;
+        /// <summary>
+        /// If the TrailBurger has cheese
+        /// </summary>
+        public bool Cheese
+        {
+            get { return cheese; }
+            set { cheese = value; }
+        }
+
+        /// <summary>
+        /// The price of the TrailBurger
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 4.50;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the TrailBurger
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 288;
+            }
+        }
+
+        /// <summary>
+        /// Special instructions for the preparation of the TrailBurger
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!bun) instructions.Add("hold bun");
+                if (!ketchup) instructions.Add("hold ketchup");
+                if (!mustard) instructions.Add("hold mustard");
+                if (!pickle) instructions.Add("hold pickle");
+                if (!cheese) instructions.Add("hold cheese");
+
+                return instructions;
+            }
+        }
+
+
+    }
+}
