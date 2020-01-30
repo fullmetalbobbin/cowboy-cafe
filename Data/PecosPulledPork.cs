a6df77736557fb37e588b0e491d619f4f5e71dc3
Amanda Dreesen <amandadreesen@ALaptopHasNoName.local>
2020-01-29 19:00:42 -0600

    added pecospulledpork class and tested

---
+++ Data/PecosPulledPork.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CowboyCafe.Data
+{
+    public class PecosPulledPork
+    {
+
+        private bool bread = true;
+        /// <summary>
+        /// If the PecosPulledPork sammich has bread
+        /// </summary>
+        public bool Bread
+        {
+            get { return bread; }
+            set { bread = value; }
+        }
+
+        private bool pickle = true;
+        /// <summary>
+        /// If the PecosPulledPork sammich has pickle
+        /// </summary>
+        public bool Pickle
+        {
+            get { return pickle; }
+            set { pickle = value; }
+        }
+
+        /// <summary>
+        /// The price of the PecosPulledPork sammich
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 5.88;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the PecosPulledPork sammich
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 528;
+            }
+        }
+
+        /// <summary>
+        /// Special instructions for the preparation of the PecosPulledPork sammich
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!bread) instructions.Add("hold bread");
+                if (!pickle) instructions.Add("hold pickle");
+
+                return instructions;
+            }
+        }
+
+
+    }
+}
