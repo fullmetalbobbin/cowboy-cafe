dc4e28d98194db4d545a8e014925076b3347c1ab
Amanda Dreesen <amandadreesen@ALaptopHasNoName.local>
2020-02-03 16:49:11 -0600

    added chilicheesefries side and commentary yum

---
+++ Data/ChilliCheeseFries.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// Class representing the ChiliCheeseFries side
+    /// </summary>
+    public class ChiliCheeseFries : Side
+    {
+        /// <summary>
+        /// Override method with switch case to return price given a size
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Large:
+                        return 3.99;
+                    case Size.Medium:
+                        return 2.99;
+                    case Size.Small:
+                        return 1.99;
+                    default:
+                        throw new NotImplementedException("Unknown size");
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// Override method with switch case to return calories given size
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                switch(Size)
+                {
+                    case Size.Large:
+                        return 610;
+                    case Size.Medium:
+                        return 524;
+                    case Size.Small:
+                        return 433;
+                    default:
+                        throw new NotImplementedException("Unknown size");
+                }
+            }
+            
+        }
+
+
+    }
+}
