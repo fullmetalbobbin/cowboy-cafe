571d210613abbc6a8d5fa6ec6869c747e703aa47
akdreesen <akdreesen@F1092-14-2102>
2020-02-26 11:35:18 -0600

    Added IOrderItem interface and attempting to fill Order - REVISIT the the "wut?"s

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,30 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * IOrderItem.cs
+ * Author: Amanda Dreesen
+ * Description: CIS308 Cowboy Cafe
+ *              Interface for IOrderItems 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// An interface representing IOrderItems
+    /// </summary>
+    interface IOrderItem
+    {
+        /// <summary>
+        /// Gets the price of and item
+        /// </summary>
+        public abstract double Price { get; }
+
+        /// <summary>
+        /// Gets the special instructions for an item
+        /// </summary>
+        public virtual IEnumerable<string> SpecialInstructions { get; }
+
+    }
+}
--- Data/Order.cs
+++ Data/Order.cs
@@ -8,10 +8,63 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace CowboyCafe.Data
 {
-    class Order
+    /// <summary>
+    /// A class to represent food/drink/nom orders 
+    /// </summary>
+    class Order : IOrderItem
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private static uint lastOrderNumber;
+
+        private List<IOrderItem> items;
+
+        public IEnumerable<IOrderItem> Items { get; }
+
+
+        private double subtotal; 
+        /// <summary>
+        /// The subtotal of order
+        /// </summary>
+        public double Subtotal
+        {
+            get { return subtotal; }
+            set { subtotal = value; }
+        }
+
+
+        //wut?
+        public override double Price => throw new NotImplementedException();
+
+
+        //wut?
+        public override IEnumerable<string> SpecialInstructions => items.ToArray();
+
+
+        /// <summary>
+        /// Method to add an item to the order
+        /// </summary>
+        /// <param name="item">Represents the given menu item</param>
+        public void Add(IOrderItem item)
+        {
+            items.Add(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+
+        /// <summary>
+        /// Method to remove and item from the order
+        /// </summary>
+        /// <param name="item">Represents the given menu item</param>
+        private void Remove(IOrderItem item)
+        {
+            items.Remove(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+
     }
 }
