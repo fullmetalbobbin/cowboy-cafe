dd8014b0bc55effd2bbdec5a1898d1f226b74712
akdreesen <akdreesen@F1092-12-2100>
2020-03-06 17:08:42 -0600

    OnItemPropertyChanged added to Order, included OnItemPropertyChanged in Ad and Remove

--- Data/Order.cs
+++ Data/Order.cs
@@ -68,6 +68,10 @@ namespace CowboyCafe.Data
         /// <param name="item">Represents the given menu item</param>
         public void Add(IOrderItem item)
         {
+            if (item is INotifyPropertyChanged notifier)
+            {
+                notifier.PropertyChanged += OnItemPropertyChanged;
+            }
             items.Add(item);
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Subtotal"));
@@ -79,9 +83,22 @@ namespace CowboyCafe.Data
         /// <param name="item">Represents the given menu item</param>
         public void Remove(IOrderItem item)
         {
+            if (item is INotifyPropertyChanged notifier)
+            {
+                notifier.PropertyChanged += OnItemPropertyChanged;
+            }
             items.Remove(item);
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Subtotal"));
+        }
+
+        private void OnItemPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+            if (e.PropertyName == "Price")
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Subtotal"));
+            }
         }
 
     }
