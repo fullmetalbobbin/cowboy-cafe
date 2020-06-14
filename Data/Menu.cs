b5447cf16a896bd7f6e6cd4bddbe9faa219072ad
akdreesen <akdreesen@CIS-RDS1>
2020-06-14 15:09:56 -0500

    DONT YOU DARE DO THAT AGAIN YOU %&*^# REMOTE SERVER!! AAWWRRRAWR!!!!

--- Data/Menu.cs
+++ Data/Menu.cs
@@ -7,6 +7,43 @@ namespace CowboyCafe.Data
 {
     public static class Menu
     {
+        public static IEnumerable<IOrderItem> Entrees()
+        {
+            List<IOrderItem> entrees = new List<IOrderItem>();
+            entrees.Add(new AngryChicken());
+            entrees.Add(new CowpokeChili());
+            entrees.Add(new DakotaDoubleBurger());
+            entrees.Add(new PecosPulledPork());
+            entrees.Add(new RustlersRibs());
+            entrees.Add(new TexasTripleBurger());
+            entrees.Add(new TrailBurger());
+            return entrees;
+        }
+
+        public static IEnumerable<IOrderItem> Sides()
+        {
+            List<IOrderItem> sides = new List<IOrderItem>();
+            sides.Add(new BakedBeans());
+            sides.Add(new ChiliCheeseFries());
+            sides.Add(new CornDodgers());
+            sides.Add(new PanDeCampo());
+            return sides;
+        }
+
+        public static IEnumerable<IOrderItem> Drinks()
+        {
+            List<IOrderItem> drinks = new List<IOrderItem>();
+            drinks.Add(new CowboyCoffee());
+            drinks.Add(new JerkedSoda());
+            drinks.Add(new TexasTea());
+            drinks.Add(new Water());
+            return drinks;
+        }
+
+        public static IEnumerable<IOrderItem> CompleteMenu()
+        {
+            List<IOrderItem> completeMenu = new List<IOrderItem>();
+        }
 
     }
 }
